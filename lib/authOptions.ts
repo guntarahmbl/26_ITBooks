334e25bd5ea86e05f79ec6954f02d71cb8ec4ae5
guntarahmbl <guntarahambali3@gmail.com>
2024-08-23 20:42:15 +0700

    feat: add signin and connect to prisma

---
+++ lib/authOptions.ts
@@ -0,0 +1,43 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { NextAuthOptions } from "next-auth";
+import Email from "next-auth/providers/email";
+import nodemailer from "nodemailer";
+import { z } from "zod";
+import { Session, User } from 'next-auth';
+
+const prisma = new PrismaClient();
+const emailSchema = z.string().email({ message: 'Invalid email format' }) .refine((email) => email.endsWith('@mahasiswa.itb.ac.id'), { message: 'Email must end with @mahasiswa.itb.ac.id',});
+
+export const authOptions:NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        Email({
+            server: {
+              host: process.env.SMTP_HOST,
+              port: Number(process.env.SMTP_PORT),
+              auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD
+              }
+            },
+            from: process.env.EMAIL_FROM
+          }),
+    ],
+    pages: {
+      signIn : "/"
+    },
+    callbacks: {
+      async signIn({ user }) {
+        try {
+          await emailSchema.parseAsync(user.email);
+          return true;
+        } catch (error) {
+          return false
+        }
+      },
+    },
+      // pages: {
+    //   signIn: "api/auth/signin", // Use the custom sign-in page path
+    // }
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Dashboard from "../../../components/dashboard";
+
+export default function Home() {
+  return (
+    <main className="flex h-screen flex-col items-center w-[80%]">
+      <Dashboard />
+    </main>
+  );
+}
