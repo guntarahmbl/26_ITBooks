71cfb6198287d743273eb088191e96671a1d52e6
DanielDPW <150114531+DanielDPW@users.noreply.github.com>
2024-08-24 15:24:43 +0700

    refactor: finished refactoring into root/buyer

--- src/app/buyer/catalog/page.tsx
+++ src/app/buyer/catalog/page.tsx
@@ -1,7 +1,3 @@
-
-import { useSearchParams } from 'next/navigation';
-import { books } from "../../../../utils/example"; // Data buku sementara
-import Card from '../../../../components/Books/Card';
 import { db } from '../../../../lib/prisma';
 import Search from './search';
 
