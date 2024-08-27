e77e426ad11cac0fb647c257b5832f992ce51fda
guntarahmbl <guntarahambali3@gmail.com>
2024-08-27 20:16:36 +0700

    fix: type error on catalog

--- src/app/buyer/catalog/page.tsx
+++ src/app/buyer/catalog/page.tsx
@@ -4,13 +4,14 @@ import Search from './search';
 import Navbar from '@/app/components/NavBar/Navbar';
 import { authOptions } from '../../../../lib/authOptions';
 import { redirect } from 'next/navigation';
+import { Book } from '../../../../lib/type';
 async function getBooks() {
     const books = await db.catalogue.findMany()
     return books;
 }
 
 export default async function Home() {
-    const books = await getBooks();
+    const books:Book[] = await getBooks();
     const session = await getServerSession(authOptions);
     console.log(session)
     if (!session){
