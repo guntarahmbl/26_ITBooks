f391c8979ad0e9a2632c697aeb3c195f2a2b2af3
DanielDPW <150114531+DanielDPW@users.noreply.github.com>
2024-09-08 23:02:28 +0700

    fix: fixed alert appearing after refreshing page

--- src/app/components/List.tsx
+++ src/app/components/List.tsx
@@ -28,7 +28,6 @@ export default function List({ idBuku, name, price, type }: { idBuku: number, na
 
             alert('Item deleted successfully');
             router.refresh();
-            
 
         } catch (error) {
             console.error('Error:', error);
@@ -40,16 +39,17 @@ export default function List({ idBuku, name, price, type }: { idBuku: number, na
     
 
     return (
-        <Link href={type === "catalogue" ? `myproducts/details/${idBuku}`: ""}>
             <div className="flex flex-row items-center border border-black rounded-xl p-2 bg-cream my-3 hover:scale-105 duration-300 transition-all">
-                <div id="title" className="w-[35%] flex flex-row items-center">
-                    <input type="checkbox" name="" id="" className="h-5 w-5" />
-                    <Image src="/books2.svg" width={50} height={50} alt="" />
-                    <h2>{name}</h2>
-                </div>
-                <p className="flex w-[15%]">1</p>
-                <p className="w-[23%]">{price}</p>
-                <p className="w-[23%]">{price}</p>
+                <Link href={type === "catalogue" ? `myproducts/details/${idBuku}`: ""} className="flex flex-row justify-center items-center w-full">
+                    <div id="title" className="w-[35%] flex flex-row items-center">
+                        <input type="checkbox" name="" id="" className="h-5 w-5" />
+                        <Image src="/books2.svg" width={50} height={50} alt="" />
+                        <h2>{name}</h2>
+                    </div>
+                    <p className="flex w-[15%]">1</p>
+                    <p className="w-[23%]">{price}</p>
+                    <p className="w-[23%]">{price}</p>
+                </Link>
                 <button
                     type="button"
                     className="w-[2%]"
@@ -59,6 +59,6 @@ export default function List({ idBuku, name, price, type }: { idBuku: number, na
                     <Image src="/trash.svg" width={25} height={25} alt="" className="w-full" />
                 </button>
             </div>
-        </Link>
+        
     );
 }
